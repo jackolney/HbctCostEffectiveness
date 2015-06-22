4b1eddcf79c468ba39869b02d479cfc890105f52
Jack Olney <jack.olney@me.com>
2015-06-22 17:12:07 +0100

    I changed a zero...

--- script/Run.R
+++ script/Run.R
@@ -9,7 +9,7 @@ Baseline <- .Call("CallCascade",popSize, 0, 0, 0, 0, 0)
 Baseline
 
 system("date")
-popSize = 100
+popSize = 1000
 dyn.load("./src/main.so")
 
 Hbct <- .Call("CallCascade",popSize, 1, 0, 0, 0, 0)
