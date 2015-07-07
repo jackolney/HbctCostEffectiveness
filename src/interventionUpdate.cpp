0153e75fa5df0a392143aee7e1329b743981412d
Jack Olney <jack.olney11@imperial.ac.uk>
2015-07-07 15:10:55 +0100

    Minor fixes / updates to run interventions from 2016 onwards.

--- src/interventionUpdate.cpp
+++ src/interventionUpdate.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 void ScheduleHctHivTest(person * const thePerson, const double theTime, const bool poc, const bool PreArtRetention, const bool ArtRetention, const bool Adherence)
 {
-	if(thePerson->GetBirthDay() != 0 && theTime >= 14610 && theTime < 21915) {
+	if(thePerson->GetBirthDay() != 0 && theTime >= 16801.5 && theTime < 24106.5) {
 		const double diagDay = theRng->SampleExpDist(hctHivTestTime);
 		if(diagDay <= 365.25)
 			new HctHivTest(thePerson,theTime + diagDay,poc,PreArtRetention,ArtRetention,Adherence);
@@ -35,7 +35,7 @@ void ScheduleHctHivTest(person * const thePerson, const double theTime, const bo
 
 void SchedulePerpetualHctHivTest(person * const thePerson, const double theTime)
 {
-	if(thePerson->GetBirthDay() != 0 && theTime >= 14610 && theTime < 21915)
+	if(thePerson->GetBirthDay() != 0 && theTime >= 16801.5 && theTime < 24106.5)
 		new HctHivTest(thePerson,theTime + (theRng->doub() * 365.25),false,false,false,false);
 }
 
--- src/interventions.cpp
+++ src/interventions.cpp
@@ -43,8 +43,8 @@ extern int const * p_Calibration;
 void SeedInterventions(person * const thePerson)
 {
 	if(*p_NaiveHbct || *p_HbctNcd || *p_HbctNcdPreArtRetention || *p_HbctNcdRetention || *p_HbctNcdRetentionAdherence || *p_HbctFrequency || *p_Vct || *p_HbctPocCd4 || *p_Linkage || *p_PreOutreach || *p_ImprovedCare || *p_PocCd4 || *p_VctPocCd4 || *p_ArtOutreach || *p_ImmediateArt || *p_UniversalTestAndTreat || *p_Adherence || *p_Calibration) {
-		if(thePerson->GetBirthDay() < 14610)
-			new Interventions(thePerson,14610);
+		if(thePerson->GetBirthDay() < 16801.5)
+			new Interventions(thePerson,16801.5);
 		else
 			new Interventions(thePerson,thePerson->GetBirthDay());
 	}
@@ -80,8 +80,8 @@ void Interventions::Execute()
 		// Every 4 years
 		if(*p_NaiveHbct == 1)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,false,false,false);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,false,false,false);
 	}
 
 	/////////////////////
@@ -92,8 +92,8 @@ void Interventions::Execute()
 		// Every 4 years
 		if(*p_HbctNcd == 1)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,false,false,false);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,false,false,false);
 
 		pPerson->SetHctNcdCostTrigger(true);
 	}
@@ -106,8 +106,8 @@ void Interventions::Execute()
 		// Every 4 years
 		if(*p_HbctNcdPreArtRetention == 1)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,true,false,false);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,true,false,false);
 
 		pPerson->SetHctNcdCostTrigger(true);
 	}
@@ -120,8 +120,8 @@ void Interventions::Execute()
 		// Every 4 years
 		if(*p_HbctNcdRetention == 1)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,true,true,false);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,true,true,false);
 
 		pPerson->SetHctNcdCostTrigger(true);
 	}
@@ -134,8 +134,8 @@ void Interventions::Execute()
 		// Every 4 years
 		if(*p_HbctNcdRetentionAdherence == 1)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,true,true,true);
 
 		pPerson->SetHctNcdCostTrigger(true);
 	}
@@ -147,87 +147,87 @@ void Interventions::Execute()
 		// Every year
 		if(*p_HbctFrequency == 1)
 			for(size_t i=0;i<20;i++)
-				if(GetTime() <= 14610 + (i * 365.25))
-					new SeedHct(pPerson,14610 + (i * 365.25),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 365.25))
+					new SeedHct(pPerson,16801.5 + (i * 365.25),false,true,true,true);
 
 		// Every 2 years
 		if(*p_HbctFrequency == 2)
 			for(size_t i=0;i<10;i++)
-				if(GetTime() <= 14610 + (i * 730.5))
-					new SeedHct(pPerson,14610 + (i * 730.5),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 730.5))
+					new SeedHct(pPerson,16801.5 + (i * 730.5),false,true,true,true);
 
 		// Every 3 years
 		if(*p_HbctFrequency == 3)
 			for(size_t i=0;i<7;i++)
-				if(GetTime() <= 14610 + (i * 1095.75))
-					new SeedHct(pPerson,14610 + (i * 1095.75),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 1095.75))
+					new SeedHct(pPerson,16801.5 + (i * 1095.75),false,true,true,true);
 
 		// Every 4 years
 		if(*p_HbctFrequency == 4)
 			for(size_t i=0;i<5;i++)
-				if(GetTime() <= 14610 + (i * 1461))
-					new SeedHct(pPerson,14610 + (i * 1461),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 1461))
+					new SeedHct(pPerson,16801.5 + (i * 1461),false,true,true,true);
 
 		// Every 5 years
 		if(*p_HbctFrequency == 5)
 			for(size_t i=0;i<4;i++)
-				if(GetTime() <= 14610 + (i * 1826.25))
-					new SeedHct(pPerson,14610 + (i * 1826.25),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 1826.25))
+					new SeedHct(pPerson,16801.5 + (i * 1826.25),false,true,true,true);
 
 		// Every 6 years
 		if(*p_HbctFrequency == 6)
 			for(size_t i=0;i<4;i++)
-				if(GetTime() <= 14610 + (i * 2191.5))
-					new SeedHct(pPerson,14610 + (i * 2191.5),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 2191.5))
+					new SeedHct(pPerson,16801.5 + (i * 2191.5),false,true,true,true);
 
 		// Every 7 years
 		if(*p_HbctFrequency == 7)
 			for(size_t i=0;i<3;i++)
-				if(GetTime() <= 14610 + (i * 2556.75))
-					new SeedHct(pPerson,14610 + (i * 2556.75),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 2556.75))
+					new SeedHct(pPerson,16801.5 + (i * 2556.75),false,true,true,true);
 
 		// Every 8 years
 		if(*p_HbctFrequency == 8)
 			for(size_t i=0;i<3;i++)
-				if(GetTime() <= 14610 + (i * 2922))
-					new SeedHct(pPerson,14610 + (i * 2922),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 2922))
+					new SeedHct(pPerson,16801.5 + (i * 2922),false,true,true,true);
 
 		// Every 10 years
 		if(*p_HbctFrequency == 10)
 			for(size_t i=0;i<2;i++)
-				if(GetTime() <= 14610 + (i * 3652.5))
-					new SeedHct(pPerson,14610 + (i * 3652.5),false,true,true,true);
+				if(GetTime() <= 16801.5 + (i * 3652.5))
+					new SeedHct(pPerson,16801.5 + (i * 3652.5),false,true,true,true);
 
 		// 2010
 		if(*p_HbctFrequency == 11) {
 			const double HbctTime[1] = {0};
 			for(size_t i=0;i<1;i++)
-				if(GetTime() <= 14610 + HbctTime[i])
-					new SeedHct(pPerson,14610 + HbctTime[i],false,true,true,true);
+				if(GetTime() <= 16801.5 + HbctTime[i])
+					new SeedHct(pPerson,16801.5 + HbctTime[i],false,true,true,true);
 		}
 
 		// 2010, 2011
 		if(*p_HbctFrequency == 12) {
 			const double HbctTime[2] = {0,365.25};
 			for(size_t i=0;i<2;i++)
-				if(GetTime() <= 14610 + HbctTime[i])
-					new SeedHct(pPerson,14610 + HbctTime[i],false,true,true,true);
+				if(GetTime() <= 16801.5 + HbctTime[i])
+					new SeedHct(pPerson,16801.5 + HbctTime[i],false,true,true,true);
 		}
 
 		// 2010, 2011, 2014
 		if(*p_HbctFrequency == 13) {
 			const double HbctTime[3] = {0,365.25,1461};
 			for(size_t i=0;i<3;i++)
-				if(GetTime() <= 14610 + HbctTime[i])
-					new SeedHct(pPerson,14610 + HbctTime[i],false,true,true,true);
+				if(GetTime() <= 16801.5 + HbctTime[i])
+					new SeedHct(pPerson,16801.5 + HbctTime[i],false,true,true,true);
 		}
 
 		// 2010, 2011, 2014, 2020
 		if(*p_HbctFrequency == 14) {
 			const double HbctTime[4] = {0,365.25,1461,3652.5};
 			for(size_t i=0;i<4;i++)
-				if(GetTime() <= 14610 + HbctTime[i])
-					new SeedHct(pPerson,14610 + HbctTime[i],false,true,true,true);
+				if(GetTime() <= 16801.5 + HbctTime[i])
+					new SeedHct(pPerson,16801.5 + HbctTime[i],false,true,true,true);
 		}
 
 		pPerson->SetHctNcdCostTrigger(true);
@@ -251,8 +251,8 @@ void Interventions::Execute()
 	if(*p_HbctPocCd4) {
 		D(cout << "HbctPocCd4 Intervention." << endl);
 		for(size_t i=0;i<5;i++)
-			if(GetTime() <= 14610 + (i * 1461))
-				new SeedHct(pPerson,14610 + (i * 1461),true,false,false,false);
+			if(GetTime() <= 16801.5 + (i * 1461))
+				new SeedHct(pPerson,16801.5 + (i * 1461),true,false,false,false);
 		
 		if(*p_HbctPocCd4 == 1) {
 			hctProbLink = 1;
@@ -289,8 +289,8 @@ void Interventions::Execute()
 		if(*p_PreOutreach == 1) { k = 1; } else { k = 0.2; }
 		
 		for(size_t i=0;i<20;i++)
-			if(GetTime() <= 14792.625 + (i * 365.25))
-				new PreArtOutreach(pPerson,14792.625 + (i * 365.25),k);
+			if(GetTime() <= 16984.125 + (i * 365.25))
+				new PreArtOutreach(pPerson,16984.125 + (i * 365.25),k);
 	}
 	
 	/////////////////////
@@ -345,8 +345,8 @@ void Interventions::Execute()
 		if(*p_ArtOutreach == 1) { k = 1; } else { k = 0.4; }
 		
 		for(size_t i=0;i<20;i++)
-			if(GetTime() <= 14792.625 + (i * 365.25))
-				new ArtOutreach(pPerson,14792.625 + (i * 365.25),k);
+			if(GetTime() <= 16984.125 + (i * 365.25))
+				new ArtOutreach(pPerson,16984.125 + (i * 365.25),k);
 	}
 	
 	/////////////////////
@@ -367,8 +367,8 @@ void Interventions::Execute()
 		UpdateTreatmentGuidelines(pPerson,4,1);
 		
 		for(size_t i=0;i<5;i++)
-			if(GetTime() <= 14610 + (i * 1461))
-				new SeedHct(pPerson,14610 + (i * 1461),false,false,false,false);
+			if(GetTime() <= 16801.5 + (i * 1461))
+				new SeedHct(pPerson,16801.5 + (i * 1461),false,false,false,false);
 		
 		if(*p_UniversalTestAndTreat == 1) {
 			hctProbLink = 1;
@@ -393,8 +393,8 @@ void Interventions::Execute()
 
 	if(*p_Calibration) {
 		for(size_t i=0;i<20;i++)
-			if(GetTime() <= 14610 + (i * 365.25))
-				new SeedPerpetualHct(pPerson, 14610 + (i * 365.25));
+			if(GetTime() <= 16801.5 + (i * 365.25))
+				new SeedPerpetualHct(pPerson, 16801.5 + (i * 365.25));
 	}
 
 	/////////////////////
